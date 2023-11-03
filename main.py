392e38d6cb343ff8bd712e5a8f773dee26086e94
yz674 <yz674@duke.edu>
2023-11-02 22:28:21 -0400

    finalize pyspark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-"""
-Main code
-"""
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import when
 
 
 def add(a, b):
@@ -8,5 +7,26 @@ def add(a, b):
 
 
 if __name__ == "__main__":
-    result = add(2, 4)
-    print(f"The result of adding 2 and 4 is {result}")
+    # Initialize a Spark session
+    spark = SparkSession.builder.appName("example").getOrCreate()
+
+    # Load the dataset
+    data = spark.read.csv("./data.csv", header=True, inferSchema=True)
+
+    # Calculate the average salary using Spark SQL
+    data.createOrReplaceTempView("alcohol")
+    data_value = spark.sql(
+        "SELECT AVG(Data_value) AS AvgValue FROM alcohol WHERE Series_reference = 'ALCA.SAABS'"
+    )
+    data_value.show()
+
+    # Perform the data transformation
+    data = data.withColumn(
+        "Value_Group", when(data.Data_value < 100, "Less").otherwise("More")
+    )
+
+    # Show the transformed DataFrame
+    data.show()
+
+    # Stop the SparkSession
+    spark.stop()
